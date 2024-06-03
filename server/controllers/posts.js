22264768f2ce228ed31b0d9f9bcda791178fe196
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-06-03 15:03:32 +0200

    getFeedPosts and getUserPosts controllers setup

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,6 @@
 import Post from "../models/Post.js";
 import User from "../models/User.js";
+import { getUser } from "./users";
 
 /* CREATE */
 export const createPost = async (req, res) => {
@@ -23,5 +24,25 @@ export const createPost = async (req, res) => {
     res.status(201).json(post);
   } catch (err) {
     res.status(409).json({ message: err.message });
+  }
+};
+
+/* READ */
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
   }
 };
