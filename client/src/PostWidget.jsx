6903dd6deed27d15048649c5fbf4bda93513c7da
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-06-12 00:33:30 +0200

    PostWidget setup

--- client/src/PostWidget.jsx
+++
@@ -1,9 +0,0 @@
-import {
-  ChatBubbleOutlined,
-  FavoriteBorderOutlined,
-  FavoriteOutlined,
-  ShareOutlined,
-} from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
-import FlexBetween from "components/FlexBetween";
-import Friend from "components/Friend";
