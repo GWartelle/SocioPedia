409f59243377da514cf660d4bacdf80f6b6e4c8c
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-02 22:22:08 +0200

    profilePage update

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
-import MyPostWidget from "scenes/widgets/MyPostWidget";
+// import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import FriendListWidget from "scenes/widgets/FriendListWidget";
 
@@ -48,8 +48,11 @@ const ProfilePage = () => {
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
-          <MyPostWidget picturePath={user.picturePath} />
+          {/* Posting originally allowed from other people profiles,
+          Disabled for simplicity */}
+          {/* <MyPostWidget picturePath={user.picturePath} /> 
           <Box m="2rem 0" />
+          */}
           <PostsWidget userId={userId} isProfile />
         </Box>
       </Box>
