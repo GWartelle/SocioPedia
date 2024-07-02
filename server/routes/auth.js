83fa75d68cf43d254f3c8ad4b419ae88cdda2ef0
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-03 00:03:33 +0200

    Creating a deleteUser route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/auth.js";
+import { login, deleteUser } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.delete("/delete", deleteUser);
 
 export default router;
