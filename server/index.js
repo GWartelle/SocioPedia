fd0bcef76330e56b45cb37101885ee664842afb9
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-03 19:24:08 +0200

    Backend setup for deployment

--- server/index.js
+++ server/index.js
@@ -32,6 +32,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
