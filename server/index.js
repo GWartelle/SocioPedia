ca0e78aca8d03581aa5007590fbcb79dde8270f6
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-07-04 16:13:49 +0200

    Build fixes

--- server/index.js
+++ server/index.js
@@ -32,10 +32,13 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
